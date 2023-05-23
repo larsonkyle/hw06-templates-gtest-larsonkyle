6ca236f01b2a30b30aaa4ce60262dad97d8bdd54
Kyle Larson <122947142+larsonkyle@users.noreply.github.com>
2023-05-22 20:20:44 -0700

    Created driver main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
-#include "listType.h"
+#include <iostream>
 #include "stock.h"
+#include "listType.h"
 #include "stockList.h"
-using namespace std;
-int main(){
-    cout << "Hello World";
-    return 0;
+
+int main() {
+  // Create a stock object and test its functionality
+  Stock stock1("AAPL", 100.0);
+  Stock stock2("GOOGL", 200.0);
+  std::cout << "Stock 1: " << stock1 << std::endl;
+  std::cout << "Stock 2: " << stock2 << std::endl;
+  std::cout << "Stock 1 value: $" << stock1.getValue() << std::endl;
+
+  // Create a listType object and test its functionality
+  ListType<int> intList;
+  intList.insert(10);
+  intList.insert(20);
+  intList.insert(30);
+  std::cout << "Integer list: ";
+  intList.print();
+  std::cout << "List size: " << intList.getSize() << std::endl;
+
+  // Create a stockList object and test its functionality
+  StockList stockList;
+  stockList.addStock(stock1);
+  stockList.addStock(stock2);
+  std::cout << "Stock List:" << std::endl;
+  stockList.print();
+
+  return 0;
 }
